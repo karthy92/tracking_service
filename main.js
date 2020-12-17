e296fc123683a331af6d770db221dff18b3da1f5
Karthy.Chinu <karthy.chinu@samsung.com>
2020-12-17 14:52:01 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+//const _ = require('lodash');
+const Hapi = require('@hapi/hapi');
+const TrackerService = require('./tracking_service.js');
+const init = async () => {
+
+    const me = this;
+    const trackingService = new TrackerService();
+    const server = Hapi.server({
+        port: 3001,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/getOrder',
+        config: {
+         handler: (request, reply) => {return trackingService.getOrderDetails() }
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
---
+++ tracking_service.js
@@ -0,0 +1,9 @@
+class TrackingService {
+    constructor() {
+
+    }
+    async getOrderDetails(poId) {
+        return {details: []}
+    }
+}
+module.exports = TrackingService;
